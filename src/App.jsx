fef92dbf0c63c3266fd5d09f19596ca2e1e05ef2
Ben <61493854+bengera@users.noreply.github.com>
2025-05-09 15:20:15 +0700

    render product list and move image assets to public folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,38 @@
+import { useState } from "react";
+import data from "../src/data.json";
+
 function App() {
+  const [products, setProducts] = useState(data);
+
   return (
     <div className="container">
       <h1>Desserts</h1>
       <main>
         <div className="product-list">
-          <div className="product-list__content">
-            <div className="product-item">
-              <img
-                className="product-item__image"
-                src="src\assets\images\image-waffle-desktop.jpg"
-                alt="waffle"
-              />
-              <button>
+          {products.map((item, idx) => (
+            <div className="product-item" key={idx}>
+              <div className="product-item__img-container">
                 <img
-                  src="src\assets\images\icon-add-to-cart.svg"
-                  alt="add-to-cart"
+                  className="product-item__image"
+                  src={item.image.desktop}
+                  alt="waffle"
                 />
-                Add to Cart
-              </button>
-              <p className="product-item__type">Waffle</p>
-              <h2 className="product-item__name">Waffle with Berries</h2>
-              <p className="product-item__cost">$6.50</p>
+
+                <button className="product-item__button">
+                  <img
+                    src="\assets\images\icon-add-to-cart.svg"
+                    alt="add-to-cart"
+                  />
+                  Add to Cart
+                </button>
+              </div>
+              <div className="product-item__info-container">
+                <p className="product-item__type">{item.category}</p>
+                <h2 className="product-item__name">{item.name}</h2>
+                <p className="product-item__cost">${item.price.toFixed(2)}</p>
+              </div>
             </div>
-          </div>
+          ))}
         </div>
       </main>
     </div>
