4a2fec7ede20ac607c66b8d4b6a55e03a7bd8248
Ben <61493854+bengera@users.noreply.github.com>
2025-05-09 16:50:54 +0700

    Add shopping cart element with basic styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,53 @@
 import { useState } from "react";
-import data from "../src/data.json";
+import data from "./data.json";
 
 function App() {
   const [products, setProducts] = useState(data);
 
   return (
     <div className="container">
-      <h1>Desserts</h1>
-      <main>
-        <div className="product-list">
-          {products.map((item, idx) => (
-            <div className="product-item" key={idx}>
-              <div className="product-item__img-container">
-                <img
-                  className="product-item__image"
-                  src={item.image.desktop}
-                  alt="waffle"
-                />
-
-                <button className="product-item__button">
+      <div className="menu">
+        <h1>Desserts</h1>
+        <main>
+          <div className="product-list">
+            {products.map((item, idx) => (
+              <div className="product-item" key={idx}>
+                <div className="product-item__img-container">
                   <img
-                    src="\assets\images\icon-add-to-cart.svg"
-                    alt="add-to-cart"
+                    className="product-item__image"
+                    src={item.image.desktop}
+                    alt="waffle"
                   />
-                  Add to Cart
-                </button>
+
+                  <button className="product-item__button">
+                    <img
+                      src="\assets\images\icon-add-to-cart.svg"
+                      alt="add-to-cart"
+                    />
+                    Add to Cart
+                  </button>
+                </div>
+                <div className="product-item__info-container">
+                  <p className="product-item__type">{item.category}</p>
+                  <h2 className="product-item__name">{item.name}</h2>
+                  <p className="product-item__cost">${item.price.toFixed(2)}</p>
+                </div>
               </div>
-              <div className="product-item__info-container">
-                <p className="product-item__type">{item.category}</p>
-                <h2 className="product-item__name">{item.name}</h2>
-                <p className="product-item__cost">${item.price.toFixed(2)}</p>
-              </div>
-            </div>
-          ))}
+            ))}
+          </div>
+        </main>
+      </div>
+      <div className="cart">
+        <h2 className="cart__heading">Your Cart (0)</h2>
+        <div className="cart-empty">
+          <img
+            className="cart-empty__img"
+            src="assets/images/illustration-empty-cart.svg"
+            alt="empty cart"
+          />
+          <p className="cart-empty__text">Your added items will appear here</p>
         </div>
-      </main>
+      </div>
     </div>
   );
 }
