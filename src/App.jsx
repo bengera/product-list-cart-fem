9916acd412effb4a15d14857b1c7c2404857e357
Ben <61493854+bengera@users.noreply.github.com>
2025-05-09 12:27:49 +0700

    Add basic styling to product item

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,32 @@
 function App() {
-  return <h1>Product Shopping Cart🛒</h1>;
+  return (
+    <div className="container">
+      <h1>Desserts</h1>
+      <main>
+        <div className="product-list">
+          <div className="product-list__content">
+            <div className="product-item">
+              <img
+                className="product-item__image"
+                src="src\assets\images\image-waffle-desktop.jpg"
+                alt="waffle"
+              />
+              <button>
+                <img
+                  src="src\assets\images\icon-add-to-cart.svg"
+                  alt="add-to-cart"
+                />
+                Add to Cart
+              </button>
+              <p className="product-item__type">Waffle</p>
+              <h2 className="product-item__name">Waffle with Berries</h2>
+              <p className="product-item__cost">$6.50</p>
+            </div>
+          </div>
+        </div>
+      </main>
+    </div>
+  );
 }
 
 export default App;
