f7a501b3c54bbf33ed3a8e2d4a78eac184aca9f8
Ben <61493854+bengera@users.noreply.github.com>
2025-05-12 16:54:40 +0700

    Add content styling for when cart isnt empty

--- src/App.jsx
+++ src/App.jsx
@@ -39,13 +39,31 @@ function App() {
       </div>
       <div className="cart">
         <h2 className="cart__heading">Your Cart (0)</h2>
-        <div className="cart-empty">
+        {/* <div className="cart-empty">
           <img
             className="cart-empty__img"
             src="assets/images/illustration-empty-cart.svg"
             alt="empty cart"
           />
           <p className="cart-empty__text">Your added items will appear here</p>
+        </div> */}
+        <div className="cart-items">
+          <div className="cart-items__total">
+            <p>Order Total</p>
+            <p className="cart-items__cost-total">$46.50</p>
+          </div>
+          <div className="environment-message">
+            <img
+              className="environment-icon"
+              src="/assets/images/icon-carbon-neutral.svg"
+              alt="tree icon"
+            />
+            <p className="environment-message__text">
+              This is a <span style={{ fontWeight: 500 }}>carbon-neutral </span>
+              delivery
+            </p>
+          </div>
+          <button className="btn-confirm">Confirm Order</button>
         </div>
       </div>
     </div>
