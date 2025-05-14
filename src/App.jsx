ed7fdc57dfc6e7f9497545b2883f84f189c48950
Ben <61493854+bengera@users.noreply.github.com>
2025-05-14 14:59:01 +0700

    Add conditional rendering of 'add to cart' or 'quantity control button depending on if the item is in the cart'

--- src/App.jsx
+++ src/App.jsx
@@ -44,33 +44,60 @@ function ProductList({ products, cartItems, setCartItems }) {
 
   return (
     <div className="product-list">
-      {products.map((item, idx) => (
-        <div className="product-item" key={idx}>
-          <div className="product-item__img-container">
-            <img
-              className="product-item__image"
-              src={item.image.desktop}
-              alt="waffle"
-            />
+      {products.map((item, idx) => {
+        const isInCart = cartItems.some(
+          (cartItem) => cartItem.id === item.name
+        );
 
-            <button
-              className="button product-item__button"
-              onClick={() => handleAddItem(item, idx)}
-            >
+        return (
+          <div className="product-item" key={idx}>
+            <div className="product-item__img-container">
               <img
-                src="\assets\images\icon-add-to-cart.svg"
-                alt="add-to-cart"
+                className="product-item__image"
+                src={item.image.desktop}
+                alt={item.name}
               />
-              Add to Cart
-            </button>
+
+              {isInCart ? (
+                <div className="button quantity-button">
+                  <button className="btn-quantity__decrement">
+                    <img
+                      className="quantity-icon"
+                      src="/assets/images/icon-decrement-quantity.svg"
+                      alt=""
+                    />
+                  </button>
+                  <span>1</span>
+                  <button className="btn-quantity__increment">
+                    <img
+                      className="quantity-icon"
+                      src="/assets/images/icon-increment-quantity.svg"
+                      alt=""
+                    />
+                  </button>
+                </div>
+              ) : (
+                <button
+                  className="button product-item__button"
+                  onClick={() => handleAddItem(item, idx)}
+                >
+                  <img
+                    src="\assets\images\icon-add-to-cart.svg"
+                    alt="add-to-cart"
+                  />
+                  Add to Cart
+                </button>
+              )}
+            </div>
+
+            <div className="product-item__info-container">
+              <p className="product-item__type">{item.category}</p>
+              <h2 className="product-item__name">{item.name}</h2>
+              <p className="product-item__cost">${item.price.toFixed(2)}</p>
+            </div>
           </div>
-          <div className="product-item__info-container">
-            <p className="product-item__type">{item.category}</p>
-            <h2 className="product-item__name">{item.name}</h2>
-            <p className="product-item__cost">${item.price.toFixed(2)}</p>
-          </div>
-        </div>
-      ))}
+        );
+      })}
     </div>
   );
 }
