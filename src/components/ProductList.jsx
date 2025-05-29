05d5311065547b3dbf01a341efd313e4c048aab4
Ben <61493854+bengera@users.noreply.github.com>
2025-05-29 13:01:54 +0700

    Add hover effect to quantity icons

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -78,11 +78,16 @@ export function ProductList({ products, cartItems, setCartItems }) {
                     className="btn-quantity__decrement"
                     onClick={() => handleDecrement(item)}
                   >
-                    <img
+                    <svg
                       className="quantity-icon"
-                      src="/assets/images/icon-decrement-quantity.svg"
-                      alt=""
-                    />
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="10"
+                      height="2"
+                      fill="none"
+                      viewBox="0 0 10 2"
+                    >
+                      <path fill="currentColor" d="M0 .375h10v1.25H0V.375Z" />
+                    </svg>
                   </button>
                   <span>
                     {cartItems.find((cartItem) => cartItem.id === item.name)
@@ -92,11 +97,19 @@ export function ProductList({ products, cartItems, setCartItems }) {
                     className="btn-quantity__increment"
                     onClick={() => handleIncrement(item)}
                   >
-                    <img
+                    <svg
                       className="quantity-icon"
-                      src="/assets/images/icon-increment-quantity.svg"
-                      alt=""
-                    />
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="10"
+                      height="10"
+                      fill="none"
+                      viewBox="0 0 10 10"
+                    >
+                      <path
+                        fill="currentColor"
+                        d="M10 4.375H5.625V0h-1.25v4.375H0v1.25h4.375V10h1.25V5.625H10v-1.25Z"
+                      />
+                    </svg>
                   </button>
                 </div>
               ) : (
